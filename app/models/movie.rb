fa1a0d339bea2bb81add04697c7dd9dd8f13dcbd
Girish Balakrishnan <guru_g@berkeley.edu>
2012-02-04 02:14:16 -0800

    fixing deployment issue

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    all(:group => "rating").map { |m| m.rating }
+    find(:all, :select => "rating", :group => "rating").map { |m| m.rating }
   end
 end
